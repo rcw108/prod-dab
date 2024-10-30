0860ef7e0df4418b98ed14e2ebdbfa9f8419d98d
dmiroshnichenko02 <danil.miroshnichenko2022@gmail.com>
2024-10-30 15:03:44 +0200

    global error boundary

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -19,23 +19,27 @@ const SingleProductPage: FC<{ params: { slug: string } }> = async ({
 	const product: WooCommerceSingleProduct = await getSingleProductBySlug(
 		params.slug
 	)
+	console.log(product)
 
 	if (product.type !== 'variable' && product.type !== 'bundle') {
 		const pageTemplate: SimpleSingle = await fetch(simpleSingleProductUrl)
 			.then(res => res.json())
 			.catch(err => console.log(err))
+		console.log(pageTemplate)
 		return <SimpleSinglePage template={pageTemplate} data={product} />
 	}
 	if (product.type === 'variable') {
 		const pageTemplate: VariableSingle = await fetch(variableSingleProductUrl)
 			.then(res => res.json())
 			.catch(err => console.log(err))
+		console.log(pageTemplate)
 		return <VariableSinglePage template={pageTemplate} data={product} />
 	}
 	if (product.type === 'bundle') {
 		const pageTemplate: BundleSingle = await fetch(bundleSingleProductUrl)
 			.then(res => res.json())
 			.catch(err => console.log(err))
+		console.log(pageTemplate)
 		return <BundleSinglePage template={pageTemplate} data={product} />
 	}
 	if (!product) {
