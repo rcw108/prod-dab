54461c93d927620b6ea913018c447a0790a1b208
dmiroshnichenko02 <danil.miroshnichenko2022@gmail.com>
2024-10-30 20:57:57 +0200

    new peek section skeleton loader

--- src/components/ui/home/newPeekSection/NewPeekSection.tsx
+++ src/components/ui/home/newPeekSection/NewPeekSection.tsx
@@ -11,6 +11,7 @@ import ReactHtmlParser from 'react-html-parser'
 import Description from '../../headings/Description'
 import SmallHeading from '../../headings/SmallHeading'
 import SubHeading from '../../headings/SubHeading'
+import SkeletonLoader from '../../SkeletonLoader'
 import styles from './NewPeekSection.module.scss'
 
 interface INewPeekSection extends Pick<HomeACF, 'title_pr' | 'text_pr'> {}
@@ -29,6 +30,35 @@ const NewPeekSection: FC<INewPeekSection> = ({ text_pr, title_pr }) => {
 	const bundleProducts = allProducts?.filter(
 		product => product.type === 'bundle'
 	)
+
+	if (isLoading)
+		return (
+			<div className={styles.skeletonPeek}>
+				<div className='container'>
+					<div className={styles.top}>
+						<SkeletonLoader count={1} height={50} width={'100%'} />
+						<SkeletonLoader
+							className='mt-5'
+							count={1}
+							height={30}
+							width={'70%'}
+						/>
+					</div>
+					<div className={styles.grid}>
+						{Array(4)
+							.fill(0)
+							.map((_, index) => (
+								<div className={styles.skeletonItem} key={index}>
+									<SkeletonLoader height={400} width={'100%'} />
+								</div>
+							))}
+					</div>
+					<div className={styles.skeletonBtn}>
+						<SkeletonLoader count={1} width={300} height={60} />
+					</div>
+				</div>
+			</div>
+		)
 
 	return (
 		<section className={styles.peek}>
